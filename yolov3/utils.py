61eabc604599c90a4c7dc26cb55cdf6c41b16821
Paranite <gedikai@gmail.com>
2021-05-08 19:21:01 +0300

    upd

--- yolov3/utils.py
+++ yolov3/utils.py
@@ -5,7 +5,7 @@ import colorsys
 import numpy as np
 import tensorflow as tf
 from yolov3.configs import *
-from yolov3.yolov import *
+from yolov3.yolo import *
 from tensorflow.python.saved_model import tag_constants
 from color_detection.color_detector import ColorDetector
 import csv
